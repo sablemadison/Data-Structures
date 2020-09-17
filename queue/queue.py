25f940c434e0460a5a7505d2db735ce077a2141b
Sable <sablechilds@yahoo.com>
2020-09-17 19:39:44 -0400

    queues-using-arrays-queues-using-LL

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,65 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if len(self.storage) == 0:
+#             return None
+#         else:
+#             return self.storage.pop(0)
+
 class Queue:
+    class Node:
+        def __init__ (self, value, next_node = None):
+            self.value = value
+            self.next_node = next_node
+
+        def set_next(self, new_next):
+            self.next_node = new_next
+
+        def get_value(self):
+            return self.value
+
+        def get_next(self): # forgotten def get_next caused Attribute Error
+            return self.next_node
+    
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.head = None
+        self.tail = None
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        new_node = self.Node(value)
+        self.size += 1
+        if self.head is None and self.tail is None: # if statement was incorrect: 
+            self.head = new_node                    # 'if self.head and self.tail is None:' results in AttributeError: 'NoneType' object has no attribute 'set_next'
+            self.tail = new_node                    # results in AttributeError: 'NoneType' object has no attribute 'set_next'
+       
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return None
+        else:
+            result = self.head.get_value()
+            self.head = self.head.get_next()
+            self.size -= 1
+            return result
+
+
+       
